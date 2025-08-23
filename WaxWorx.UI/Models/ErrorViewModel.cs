da4b239040c4cd076b080a18a4529a6fe39c61b5
Raven Software <51460158+darrenflynn72@users.noreply.github.com>
2025-08-23 14:47:19 -0400

    Initial project creation

---
+++ WaxWorx.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WaxWorx.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
