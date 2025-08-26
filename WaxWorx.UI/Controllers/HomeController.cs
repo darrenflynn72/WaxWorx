935ab95671e6bdcfe39f7b61ec5cf388124e2e0c
Raven Software <51460158+darrenflynn72@users.noreply.github.com>
2025-08-25 21:33:09 -0400

    Replaced Chart.js with Google Charts

--- WaxWorx.UI/Controllers/HomeController.cs
+++ WaxWorx.UI/Controllers/HomeController.cs
@@ -142,6 +142,7 @@ namespace WaxWorx.Controllers
                     Count = g.Count()
                 })
                 .OrderByDescending(g => g.Count)
+                .Take(10) // JUST RETURN TOP 10 GENRES
                 .ToList();
 
             var result = Json(genreCounts);
